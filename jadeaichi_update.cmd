9c4eecac013c537eb268fa0852355c74df38a5df
pyjhhh <pyjhhh@gmail.com>
2019-02-17 05:19:50 +0800

    AA

--- jadeaichi_update.cmd
+++ jadeaichi_update.cmd
@@ -4,7 +4,7 @@ set /p commit_msg=Please input commit message:
 :: show git status
 git status
 :: add all changing
-git add -A
+git add .
 :: local commit
 git commit -m "%commit_msg%"
 :: push to remote repository
